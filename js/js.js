8ba5836342e2b79e1768f3f09b5b58a3d1f3e7f0
Kate_Style <e.ivanova.93@bk.ru>
2022-07-07 23:54:56 +0300

    +

---
+++ js/js.js
@@ -0,0 +1,29 @@
+/*function func1() {
+    var a = 1;
+    var b = 2;
+
+    var sum = a + b;
+
+    console
+}
+func1();*/
+
+/*var name = prompt("Enter your name")
+alert("Hi " + name);*/
+
+//var tf = +prompt("t по Фаренгейту");
+
+
+
+
+function func1() {
+    var tc = +prompt("t по Цельсию")
+    var tf = (9 / 5) * tc + 32;
+
+    var sum = tc + tf;
+
+    console
+}
+func1();
+
+alert("sum");
